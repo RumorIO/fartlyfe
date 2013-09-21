613a1b932416196a43a17d58f174d6af2553c4e6
Zach White <zacharystuartwhite@gmail.com>
2013-09-21 14:53:23 -0700

    Added front page placeholder

--- fartlyfe/urls.py
+++ fartlyfe/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
+    
+    url(r'^$', TemplateView.as_view(template_name='soon.html'), name="soon"),
 
 )
